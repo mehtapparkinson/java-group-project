68299d63f417a0511ed6e2592e9d6e9c40d3f68a
zuzannaboguslawska <150298188+zuzannaboguslawska@users.noreply.github.com>
2024-09-27 19:32:32 +0100

    unit test for delete endpoint with some to dos

--- src/test/java/com/cfg_java_team_jason/java_group_project/controller/MovieControllerTests.java
+++ src/test/java/com/cfg_java_team_jason/java_group_project/controller/MovieControllerTests.java
@@ -7,6 +7,11 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @WebMvcTest(MovieController.class)
 public class MovieControllerTests {
 
@@ -25,10 +30,22 @@ public class MovieControllerTests {
     public void when_updateMovie() throws Exception {
         // PutMapping endpoint test
     }
+
+    // delete tests section
     @Test
-    public void when_deleteMovie() throws Exception {
-        // DeleteMapping endpoint test
+    public void when_deleteMovie_then_returnSuccess() throws Exception {
+        int movieId = 1;
+        doNothing().when(movieRepository).deleteById(movieId);
+
+        mockMvc.perform(delete("/movies/{movieId}", movieId))
+                .andExpect(status().isOk());
+
+        verify(movieRepository).deleteById(movieId);
     }
+
+    //TODO: mock behaviour when movie doesn't exist, invalid movie id like string
+
+
     @Test
     public void when_addedMovie() throws Exception {
         // PostMapping endpoint test
